1095a0ef91b519707667b024806b61206c335ab1
Yadagalla Jaswanth Kumar <yadagallajaswanthkumar98@gmail.com>
2017-03-15 14:17:12 +0530

    Add files via upload

--- 01.cpp
+++ 01.cpp
@@ -1,7 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(){
+//template <typename indentifier> ret_type fun_name(params)
 
+template <typename myType> void display(myType x){
+  cout << "You have passed, " << x << endl;
+}
+
+int main(){
+  display(56);
+  display(1.4645);
+  display("I am Jesse.");
   return 0;
 }
